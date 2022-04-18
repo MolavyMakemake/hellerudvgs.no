320a787024770d5e834dfcf642ff0b76489fd7f7
MolavyMakemake <molavy.business@gmail.com>
2022-04-18 19:59:17 +0200

    new website

---
+++ gp-links.js
@@ -0,0 +1,15 @@
+//SPILL
+let spList = [
+    {id:'adv-ttt',ttl:'TIC... TAC... TOE...',url:"..\\tictac\\main.html",},
+    {id:'smooth-snake',ttl:'Snake',url:'http://hellerudvgs.no/Snake/Snake.html'},
+    {id:'3-pa-rad',ttl:'Tre På Rad',url:'http://hellerudvgs.no/3p%C3%A5Rad/3p%C3%A5Rad.html'},
+    
+]
+
+//PROGRAMMER
+
+let prList = [
+    {id:'adv-ttt',ttl:'TIC... TAC... TOE...',url:"..\\tictac\\",},
+    {id:'smooth-snake',ttl:'Snake',url:'http://hellerudvgs.no/Snake/'},
+    {id:'3-pa-rad',ttl:'Tre På Rad',url:'http://hellerudvgs.no/3p%C3%A5Rad/'}
+]
